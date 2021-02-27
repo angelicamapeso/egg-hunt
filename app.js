a99e3f28ddab3739c06e4f0773084330e2e1eac5
Angelica Mapeso <angelica.mapeso@gmail.com>
2021-02-27 16:38:45 -0500

    Set up server, serve index.html

---
+++ app.js
@@ -0,0 +1,22 @@
+// Server setup
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+// Websocket
+const io = require("socket.io")(server);
+
+// Set port (process.env.PORT if deploying)
+const PORT = process.env.PORT || 8080;
+
+// Set static files
+app.use(express.static(__dirname + "/public"));
+
+// Server index.html at root route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Start server
+server.listen(PORT, function () {
+  console.log(`App listening on: localhost:${PORT}`);
+});
