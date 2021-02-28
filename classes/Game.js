391081ff240f7cb04d8f41c854acdcd76128881a
Angelica Mapeso <angelica.mapeso@gmail.com>
2021-02-28 18:26:26 -0500

    Update addPlayer to return player obj

--- classes/Game.js
+++ classes/Game.js
@@ -8,12 +8,12 @@ class Game {
 
 // Returns true if player successfully added and false if not
 Game.prototype.addPlayer = function (socketID, position) {
+  let newPlayer = null;
   if (this.players.length < 2) {
-    this.players.push(new Player(socketID, position));
-    return true;
-  } else {
-    return false;
+    newPlayer = new Player(socketID, position);
+    this.players.push(newPlayer);
   }
+  return newPlayer;
 };
 
 Game.prototype.removePlayer = function (socketID) {
