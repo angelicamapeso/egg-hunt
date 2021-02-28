1e19a5e2eaeea1abd3f6918abfdf66f908cbd459
Angelica Mapeso <angelica.mapeso@gmail.com>
2021-02-27 22:05:29 -0500

    Render second player in room

--- public/js/game.js
+++ public/js/game.js
@@ -6,8 +6,23 @@ AFRAME.registerComponent("game", {
 
     // update game state
     this.game = null;
+    this.player2 = null;
     this.updateGame = (game) => {
       this.game = JSON.parse(game);
+
+      if (this.game.players.length > 1) {
+        // Create player 2
+        if (!this.player2) {
+          this.player2 = document.createElement("a-entity");
+          this.player2.setAttribute("player2", "");
+          el.appendChild(this.player2);
+        }
+      } else {
+        if (this.player2) {
+          this.player2.remove();
+          this.player2 = null;
+        }
+      }
     };
 
     // hide scene
