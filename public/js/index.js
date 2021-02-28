ae93d4ee4656cd6d3068ce88ba7bc7c3eb639fe2
Angelica Mapeso <angelica.mapeso@gmail.com>
2021-02-28 18:58:18 -0500

    Add socket event constants

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,10 @@
 // Defining global variables
 const socket = io();
+
+// Websocket events
+const FULL_ROOM = "full-room";
+const JOIN = "join";
+const GAME_OBJECT = "game-object";
+const ADD_PLAYER = "add-player";
+const REMOVE_PLAYER = "remove-player";
+const UPDATE_POSITION = "update-position";
