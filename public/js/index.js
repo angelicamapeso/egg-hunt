875f1e70c269954e858f481890a15e176fdbe4aa
Angelica Mapeso <angelica.mapeso@gmail.com>
2021-02-28 21:11:20 -0500

    Add update rotation event

--- public/js/index.js
+++ public/js/index.js
@@ -8,3 +8,4 @@ const GAME_OBJECT = "game-object";
 const ADD_PLAYER = "add-player";
 const REMOVE_PLAYER = "remove-player";
 const UPDATE_POSITION = "update-position";
+const UPDATE_ROTATION = "update-rotation";
--- socket-events.js
+++ socket-events.js
@@ -5,4 +5,5 @@ module.exports = {
   ADD_PLAYER: "add-player",
   REMOVE_PLAYER: "remove-player",
   UPDATE_POSITION: "update-position",
+  UPDATE_ROTATION: "update-rotation",
 };
