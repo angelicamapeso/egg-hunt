03ce14022f132075b8bf731f94b5410715c16dcb
Angelica Mapeso <angelica.mapeso@gmail.com>
2021-02-28 21:44:58 -0500

    Add rotation to player.js

--- classes/Player.js
+++ classes/Player.js
@@ -1,13 +1,19 @@
 class Player {
-  constructor(socketID, position) {
+  constructor(socketID, position, rotation) {
     this.id = socketID;
     // position is the player's 3D position in space - [x, y, z]
     this.position = position;
+    // rotation is player's quaternion rotation - [x, y, z, w]
+    this.rotation = rotation;
   }
 }
 
 Player.prototype.updatePosition = function (newPosition) {
   this.position = newPosition;
+};
+
+Player.prototype.updateRotation = function (newRotation) {
+  this.rotation = newRotation;
 };
 
 module.exports = Player;
