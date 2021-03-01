e24836d8dcff54feace320fa13daa7f405b6648e
Angelica Mapeso <angelica.mapeso@gmail.com>
2021-02-28 20:21:33 -0500

    Move func outisde init

--- public/js/player2.js
+++ public/js/player2.js
@@ -11,20 +11,23 @@ AFRAME.registerComponent("player2", {
 
     this.el.setAttribute("material", { color: "#FFC65D" });
 
-    this.updatePosition = (player) => {
-      if (player.id !== socket.id) {
-        this.el.object3D.position.set(
-          player.position[0],
-          player.position[1],
-          player.position[2]
-        );
-      }
-    };
+    // Binding
+    this.updatePosition = this.updatePosition.bind(this);
 
     socket.on(UPDATE_POSITION, this.updatePosition);
   },
 
   remove: function () {
     socket.removeEventListener(UPDATE_POSITION, this.updatePosition);
+  },
+
+  updatePosition: function (player) {
+    if (player.id !== socket.id) {
+      this.el.object3D.position.set(
+        player.position[0],
+        player.position[1],
+        player.position[2]
+      );
+    }
   },
 });
