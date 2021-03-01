e0247a65e81b9d915bcb31359e9c546431659c74
Angelica Mapeso <angelica.mapeso@gmail.com>
2021-02-28 20:19:34 -0500

    Move functions outside init

--- public/js/player.js
+++ public/js/player.js
@@ -3,32 +3,35 @@ AFRAME.registerComponent("player", {
     // Establish connection
     this.socket = socket;
 
-    // Update position
-    this.getPosition = () => {
-      let position = new THREE.Vector3();
-      return this.el.object3D.getWorldPosition(position);
-    };
-
-    // Send position
-    this.sendPosition = () => {
-      const currentPosition = this.getPosition();
-      if (this.position && !this.position.equals(currentPosition)) {
-        this.position = currentPosition;
-        this.socket.emit(UPDATE_POSITION, [
-          this.position.x,
-          this.position.y,
-          this.position.z,
-        ]);
-      }
-    };
+    // Bindings
+    this.sendPosition = this.sendPosition.bind(this);
+    this.getPosition = this.getPosition.bind(this);
 
     // Update position when first join
     this.position = this.getPosition();
-    console.log(this.position);
     this.socket.emit(JOIN, [this.position.x, this.position.y, this.position.z]);
   },
 
   tick: function () {
     this.sendPosition();
+  },
+
+  // To emit position if player has moved
+  sendPosition: function () {
+    const currentPosition = this.getPosition();
+    if (this.position && !this.position.equals(currentPosition)) {
+      this.position = currentPosition;
+      this.socket.emit(UPDATE_POSITION, [
+        this.position.x,
+        this.position.y,
+        this.position.z,
+      ]);
+    }
+  },
+
+  // Helper functions
+  getPosition: function () {
+    let position = new THREE.Vector3();
+    return this.el.object3D.getWorldPosition(position);
   },
 });
